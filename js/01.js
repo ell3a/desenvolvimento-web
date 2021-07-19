0d61337601b6dedfdbb2141a6625977440de8e55
Leandro Vieira <81088013+ell3a@users.noreply.github.com>
2021-07-19 13:01:54 -0300

    atualizações

---
+++ js/01.js
@@ -0,0 +1,12 @@
+/*let nome =  "Leandro";
+let sobrenome =  " Vieira dos Santos";
+let resultado = `Meu nome é ${nome+sobrenome}`
+
+console.log(resultado);
+*/
+
+// .exit: sai do terminal node
+// Ctrl + L: limpa terminal node
+
+var n = 'Leandro Vieira'
+console.log(n)
